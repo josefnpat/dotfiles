08abab238fcfc1d7176925a8253f665bbc5c1c8c
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-21 16:54:53 -0400

    Added server config file

---
+++ setup-server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+apt-get install tasksel
+tasksel install lamp-server
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ./setup-software.sh
+./setup-server.sh
 ./setup-config.sh
 #./setup-laptop.sh
 ./setup-scripts.sh
