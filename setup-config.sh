69661951a72b7bc0324ccb7d93de76fb3f427971
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-21 14:51:27 -0400

    Adding gedit config

--- setup-config.sh
+++ setup-config.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/config/conkyrc ~/.conkyrc
 ln -s ~/dotfiles/config/nanorc ~/.nanorc
 ln -s ~/dotfiles/config/openbox ~/.config/openbox
 ln -s ~/dotfiles/config/gnome-terminal/ ~/.gconf/apps/gnome-terminal
+ln -s ~/dotfiles/config/gedit-2/ ~/.gconf/apps/gedit-2
