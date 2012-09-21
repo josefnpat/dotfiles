41f1d40c634c4f3b33d9fb39eb67028d832f6493
Seppi <josefnpat@hotmail.com>
2012-09-21 11:42:53 -0400

    Updates

--- setup-software.sh
+++ setup-software.sh
@@ -2,6 +2,8 @@
 
 # init to home folder
 cd ~
+# alternate binaries
+mkdir ~/bin -p
 
 # ppa's
 apt-add-repository ppa:bartbes/love-stable -y
@@ -10,19 +12,29 @@ apt-add-repository ppa:team-xbmc/ppa -y
 apt-get update -y
 
 # openbox
-apt-get install openbox obconf obmenu grun -y
+# I think obkey got added to the ubuntu 12.04 repos
+apt-get install openbox obconf obmenu grun obkey -y
+
+# obkey (No idea why this isn't in the repos.)
+# source
+#cd ~/bin/
+#git clone https://github.com/nsf/obkey.git
+#ln ~/bin/obkey/obkey /usr/games/ -s
 
 # stuff I like
-apt-get install chromium-browser git nvidia-settings gtk-recordmydesktop -y
+apt-get install chromium-browser nvidia-settings gtk-recordmydesktop -y
 
 # dev
-apt-get install gimp gcolor2 libreoffice kodos meld -y
+apt-get install gimp gcolor2 libreoffice kodos meld git mercurial -y
 
 # vidya gamez
 apt-get install love love-unsatble wine gweled -y
 
 # media
-apt-get install vlc herrie rhythmbox audacity xbmc -y
+apt-get install vlc mocp rhythmbox audacity xbmc -y
+
+# irc
+apt-get install irssi
 
 # don't want this!
 #apt-get remove
@@ -31,47 +43,42 @@ apt-get upgrade -y
 apt-get autoclean -y
 apt-get autoremove -y
 
-# install alternate binaries
-mkdir ~/bin
+if [ EVE ]
+then
+  # pyfa (eve online)
+  # deps
+  apt-get install git python python-wxgtk2.8 python-sqlalchemy python-matplotlib python-numpy -y
+  # source
+  cd ~/bin/
+  git clone git://www.evefit.org/pyfa.git
+  cd pyfa
+  git submodule update --init
+  cd ..
+  echo "#!/bin/sh\n~/bin/pyfa/pyfa.py" > /usr/games/pyfa
+  chmod a+x mc.sh
 
-# obkey (No idea why this isn't in the repos.)
-# deps
-apt-get install git -y
-# source
-cd ~/bin/
-git clone https://github.com/nsf/obkey.git
-ln ~/bin/obkey/obkey /usr/games/ -s
+  # gtkevemon (eve online)
+  # deps
+  apt-get install libssl1.0.0 subversion libgtk2.0-0 libgtkmm-2.4-1c2a libgtkmm-2.4-dev libxml2 libxml2-dev -y
+  # source
+  cd ~/bin/
+  svn checkout svn://svn.battleclinic.com/GTKEVEMon/trunk/gtkevemon
+  cd gtkevemon
+  make
+  make install
+fi
 
-# pyfa (eve online)
-# deps
-apt-get install git python python-wxgtk2.8 python-sqlalchemy python-matplotlib python-numpy -y
-# source
-cd ~/bin/
-git clone git://www.evefit.org/pyfa.git
-cd pyfa
-git submodule update --init
-cd ..
-echo "#!/bin/sh\n~/bin/pyfa/pyfa.py" > /usr/games/pyfa
-chmod a+x mc.sh
-
-# gtkevemon (eve online)
-# deps
-apt-get install libssl1.0.0 subversion libgtk2.0-0 libgtkmm-2.4-1c2a libgtkmm-2.4-dev libxml2 libxml2-dev -y
-# source
-cd ~/bin/
-svn checkout svn://svn.battleclinic.com/GTKEVEMon/trunk/gtkevemon
-cd gtkevemon
-make
-make install
-
-# go back to games
-#deps
-apt-get install openjdk-7 -y
-#source
-mkdir ~/games
-mkdir ~/games/minecraft
-cd ~/games/minecraft
-wget -c https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar
-echo "#!/bin/sh\njava -Xmx1024M -Xms512M -cp ~/games/minecraft/minecraft.jar net.minecraft.LauncherFrame" > mc.sh
-chmod a+x mc.sh
-ln ~/games/minecraft/mc.sh /usr/games/minecraft
+if [ MINECRAFT ]
+then
+  # go back to games
+  #deps
+  apt-get install openjdk-7 -y
+  #source
+  mkdir ~/games
+  mkdir ~/games/minecraft
+  cd ~/games/minecraft
+  wget -c https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar
+  echo "#!/bin/sh\njava -Xmx1024M -Xms512M -cp ~/games/minecraft/minecraft.jar net.minecraft.LauncherFrame" > mc.sh
+  chmod a+x mc.sh
+  ln ~/games/minecraft/mc.sh /usr/games/minecraft
+fi
