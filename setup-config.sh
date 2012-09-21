ee8fb37caa1a8054b6c22f2fc1d3797d28f42502
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-21 14:49:29 -0400

    Adding gnome-terminal config

--- setup-config.sh
+++ setup-config.sh
@@ -2,3 +2,4 @@
 ln -s ~/dotfiles/config/conkyrc ~/.conkyrc
 ln -s ~/dotfiles/config/nanorc ~/.nanorc
 ln -s ~/dotfiles/config/openbox ~/.config/openbox
+ln -s ~/dotfiles/config/gnome-terminal/ ~/.gconf/apps/gnome-terminal
