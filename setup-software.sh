864563447fc99b432a72d2942e41e5e581a215d6
Seppi <seppi@josefnpat.com>
2012-09-21 14:05:21 -0300

    irssi first, and git before obkey

--- setup-software.sh
+++ setup-software.sh
@@ -11,6 +11,9 @@ apt-add-repository ppa:ubuntu-wine/ppa -y
 apt-add-repository ppa:team-xbmc/ppa -y
 apt-get update -y
 
+# cli
+apt-get install irssi qalc git -y
+
 # openbox
 # I think obkey got added to the ubuntu 12.04 repos
 apt-get install openbox obconf obmenu grun -y
@@ -25,19 +28,16 @@ ln ~/bin/obkey/obkey /usr/games/ -s
 apt-get install chromium-browser transmission -y
 
 # desktop
-apt-get install nvidia-settings gtk-recordmydesktop qalc -y
+apt-get install nvidia-settings gtk-recordmydesktop -y
 
 # dev
-apt-get install gimp gcolor2 libreoffice kodos meld git mercurial -y
+apt-get install gimp gcolor2 libreoffice kodos meld mercurial -y
 
 # vidya
 apt-get install love wine gweled -y
 
 # media
 apt-get install vlc mocp rhythmbox audacity xbmc -y
-
-# irc
-apt-get install irssi
 
 # don't want this!
 #apt-get remove
