9703c8abfb02418dc2a967f8721735e67f550895
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-21 16:59:13 -0400

    adding skype and agave

--- setup-software.sh
+++ setup-software.sh
@@ -14,6 +14,14 @@ apt-add-repository ppa:ubuntu-wine/ppa -y
 apt-add-repository ppa:team-xbmc/ppa -y
 apt-get update -y
 
+# repos
+
+# Skype is another kind of exception. Using Skype to talk with someone else
+# who is using Skype is encouraging the other to use nonfree software. So I
+# won't use it under any circumstances. -RMS
+add-apt-repository \
+  "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
 # openbox
 # I think obkey got added to the ubuntu 12.04 repos
 apt-get install openbox obconf obmenu grun feh conky lxappearance -y
@@ -25,13 +33,13 @@ git clone https://github.com/nsf/obkey.git
 ln ~/bin/obkey/obkey /usr/games/ -s
 
 # internet
-apt-get install chromium-browser transmission pidgin -y
+apt-get install chromium-browser transmission pidgin skype -y
 
 # desktop
 apt-get install nvidia-settings gtk-recordmydesktop -y
 
 # dev
-apt-get install gimp gcolor2 libreoffice kodos meld mercurial gedit-developer-plugins -y
+apt-get install gimp gcolor2 agave libreoffice kodos meld mercurial gedit-developer-plugins php5-cli -y
 
 # vidya
 apt-get install love wine gweled -y
