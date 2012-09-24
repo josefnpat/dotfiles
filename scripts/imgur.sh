1196f3c5890f835b3fa98b810e6787be34559c3d
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-24 14:45:09 -0400

    Newlines suck! Thanks ToxicFrog!

--- scripts/imgur.sh
+++ scripts/imgur.sh
@@ -6,6 +6,11 @@ function uploadImage {
   curl -s -F "image=@$1" -F "key=486690f872c678126a2c09a9e196ce1b" http://imgur.com/api/upload.xml | grep -E -o "<original_image>(.)*</original_image>" | grep -E -o "http://i.imgur.com/[^<]*"
 }
 scrot -s "shot.png"
-uploadImage "shot.png" | xclip -selection c
+uploadImage "shot.png" | tr -d '\n' | xclip -selection c
+
+$temp | xclip -o
+
+echo $temp
+
 rm "shot.png"
 notify-send "Done"
