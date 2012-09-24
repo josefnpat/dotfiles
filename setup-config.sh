77e5529b5f9e627c84cc89fcb48bac649558c9d0
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-24 12:47:27 -0400

    Adding irssi config!

--- setup-config.sh
+++ setup-config.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/config/openbox ~/.config/openbox
 ln -s ~/dotfiles/config/gnome-terminal/ ~/.gconf/apps/gnome-terminal
 ln -s ~/dotfiles/config/gedit-2/ ~/.gconf/apps/gedit-2
 ln -s ~/dotfiles/config/gtk-3.0/ ~/.config/gtk-3.0
+ln -s ~/dotfiles/config/irssi/ ~/.irssi
