93e2b74340bfa34fed9133b83867e1b746db94de
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-24 17:01:41 -0400

    Removing .nano_history if it's already there.

--- setup-config.sh
+++ setup-config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ln -s ~/dotfiles/config/conkyrc ~/.conkyrc
 ln -s ~/dotfiles/config/nanorc ~/.nanorc
+#Avoid the nano history error
+rm ~/.nano_history
 ln -s ~/dotfiles/config/openbox ~/.config/openbox
 ln -s ~/dotfiles/config/gnome-terminal/ ~/.gconf/apps/gnome-terminal
 ln -s ~/dotfiles/config/gedit-2/ ~/.gconf/apps/gedit-2
