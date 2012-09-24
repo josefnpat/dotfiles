8cafc42d426bd16015a2fe9fcc9479e085239c45
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-24 18:43:29 -0400

    Updated to ask user what kind of stuff to install.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+
+if [ "$(whoami)" != 'root' ]; then
+  echo "You have no permission to run $0 as non-root user."
+  exit 1;
+fi
+
 ./setup-software.sh
-./setup-server.sh
 ./setup-config.sh
-#./setup-laptop.sh
 ./setup-scripts.sh
+
+read -p "Setup Laptop (y/N)? " ans
+if [[ $ans == y* ]] || [[ $ans == Y* ]]; then
+  ./setup-laptop.sh
+fi
+
+read -p "Setup Server (y/N)? " ans
+if [[ $ans == y* ]] || [[ $ans == Y* ]]; then
+  ./setup-server.sh
+fi
