a864a6cb3a7eed3d43ad85d6eb99cba36d62e5b2
josefnpat <josef.patoprsty@redbottledesign.com>
2012-09-24 12:57:21 -0400

    bashrc

--- setup-config.sh
+++ setup-config.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/config/gnome-terminal/ ~/.gconf/apps/gnome-terminal
 ln -s ~/dotfiles/config/gedit-2/ ~/.gconf/apps/gedit-2
 ln -s ~/dotfiles/config/gtk-3.0/ ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/irssi/ ~/.irssi
+ln -s ~/dotfiles/config/bashrc ~/.bashrc
