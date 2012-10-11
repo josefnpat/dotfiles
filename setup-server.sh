2896bf508af44fc7fa8a30714b54d1f0d3161133
josefnpat <josef.patoprsty@redbottledesign.com>
2012-10-11 14:35:07 -0400

    Adding drush!

--- setup-server.sh
+++ setup-server.sh
@@ -2,3 +2,17 @@
 
 apt-get install tasksel
 tasksel install lamp-server
+
+# Install of drush is a little more extensive than
+# apt-get install drush, as it's not always the newest version.
+# Bleeding edge, man! Install Gentoo.
+
+# install php's pear
+apt-get install php-pear
+# discover the proper channel
+pear channel-discover pear.drush.org
+# download drush
+pear install drush/drush
+# run drush so it can get the proper files while in root
+# (this is a one time thing.)
+drush
