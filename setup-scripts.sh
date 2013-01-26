91da82103967f732c3f36a28b48de358a4a8d816
Seppi <josefnpat@hotmail.com>
2013-01-26 16:45:14 -0500

    Adding unix time stamp script, because I can never remember the args to date

--- setup-scripts.sh
+++ setup-scripts.sh
@@ -2,3 +2,4 @@
 ln -s ~/dotfiles/scripts/naut.sh /usr/bin/naut
 ln -s ~/dotfiles/scripts/clearcache.sh /usr/bin/clearcache
 ln -s ~/dotfiles/scripts/imgur.sh /usr/bin/imgur
+ln -s ~/dotfiles/scripts/unixtime.sh /usr/bin/unixtime
