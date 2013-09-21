b467f53403cb5478127d559543c76bc48c858bb8
Josef Patoprsty <seppi@josefnpat.com>
2013-09-20 22:02:59 -0400

    Fixing png to use import Adding limit check for imgur Clearing temp space more often providing fallback for failed images that surpass MAXSIZE

--- scripts/imgur.sh
+++ scripts/imgur.sh
@@ -13,7 +13,7 @@
 
 TIME=`zenity --title "imgurgif" --entry --text="How many seconds?" --entry-text=1`
 FPS=12
-let MAXSIZE=2*1024*1024
+let MAXSIZE=2*1024
 let DELAY=1/$FPS
 let GIFDELAY=$DELAY*100
 TMP="/tmp/imgur/"
@@ -24,6 +24,11 @@ function uploadImage {
   LINK=`curl -s -F "image=@$1" -F "key=486690f872c678126a2c09a9e196ce1b" http://imgur.com/api/upload.xml | grep -E -o "<original_image>(.)*</original_image>" | grep -E -o "http://i.imgur.com/[^<]*"`
 }
 
+function clearTemp {
+  rm ${TMP}*.png
+  rm ${TMP}*.gif
+}
+
 USEPNG=false
 if [ -z "$TIME" ]
 then
@@ -31,36 +36,43 @@ then
   USEPNG=true
 fi
 
-let FRAMES=$TIME*$FPS+1
-
-echo "Fecording: ${TIME}s @ $FPS/s"
-echo "Frame Count: ${FRAMES}"
-
-SEL=`xrectsel "-s %wx%h -i :0.0+%x,%y"`
+echo "Fecording: ${TIME}s @ ${FPS}f/s"
 
 mkdir -p $TMP
+mkdir -p $TMP/fail
 
-ffmpeg -r $FPS -t $TIME -f x11grab $SEL ${TMP}%09d.png
+clearTemp
 
 if $USEPNG
 then
+  SEL=`xrectsel "%wx%h+%x+%y"`
+  import -window root -crop $SEL +repage -quality 100 ${TMP}single.png
   notify-send "Rendering PNG"
-  notify-send "Uploading PNG [ "`du -h ${TMP}1.png | awk '{print $1}'`" ]"
-  uploadImage ${TMP}1.png
+  notify-send "Uploading PNG [ "`du -h ${TMP}single.png | awk '{print $1}'`" ]"
+  uploadImage ${TMP}single.png
   echo $LINK | tr -d '\n' | xclip -selection c
   notify-send "Uploaded PNG - $LINK"
 else
+  SEL=`xrectsel "-s %wx%h -i :0.0+%x,%y"`
+  ffmpeg -r $FPS -t $TIME -f x11grab $SEL ${TMP}%09d.png
   notify-send "Rendering GIF"
   convert -layers OptimizeTransparency -delay $GIFDELAY \
     -loop 0 $(ls ${TMP}*.png | sort -V) ${TMP}imgmagik.gif
-  notify-send "Uploading GIF [ "`du -h ${TMP}imgmagik.gif | awk '{print $1}'`" ]"
-  uploadImage ${TMP}imgmagik.gif
-  echo $LINK | tr -d '\n' | xclip -selection c
-  notify-send "Uploaded GIF - $LINK"
+  FS=`du ${TMP}imgmagik.gif | awk '{print $1}'`
+  if [ "$FS" -le "$MAXSIZE" ]
+  then
+    notify-send "Uploading GIF [ "`du -h ${TMP}imgmagik.gif | awk '{print $1}'`" ]"
+    uploadImage ${TMP}imgmagik.gif
+    echo $LINK | tr -d '\n' | xclip -selection c
+    notify-send "Uploaded GIF - $LINK"
+  else
+    D=`date '+%s'`
+    notify-send "Error: Surpassed ${MAXSIZE}K Limit. ${TMP}fail/$D.gif"
+    mv ${TMP}imgmagik.gif ${TMP}fail/${D}.gif
+  fi
 fi
 
 echo $LINK
 echo $LINK >> ${TMP}imgur.log
 
-rm ${TMP}*.png
-rm ${TMP}*.gif
+clearTemp
